db3132348cd5792dcc43991651f4ab3ac84dc5cb
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-15 10:37:51 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
   end
 
   get '/recipes/new' do
