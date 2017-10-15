93b208c58440b1a9c43cd382e498c365bb7ce237
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-15 10:04:21 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,26 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
   get '/recipes/new' do
     erb :new
   end
-  
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+  end
+
+  get '/recipes/:id/edit' do
+    
+  end
+
   get '/' do
     erb :index
   end
-  
+
 end
