43d26d920e20d30c960dd246c379561072b2218e
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-15 10:19:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/recipes' do
-    
+
     @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     redirect to '/recipes/#{@recipe.id}'
 
